2ad04ad46ef4ce4fbcadc1222542a9eefb36ff7e
Linar Nasyrov <vamping111@gmail.com>
2021-08-02 15:23:18 +0300

    hw5.py has been changed

--- hw/hw5.py
+++ hw/hw5.py
@@ -3,4 +3,9 @@ inp=input("Enter your values: ").split()
 A=[int(s) for s in inp if s.isdigit()]
 def solution(A):
     return [value for value in range(min(A),max(A)) if value not in A]
-print(min(solution(A)))
+try:
+    (min(solution(A)))
+    print("The value: ",(min(solution(A))), " is missed")
+except ValueError:
+    print("Enter list with empty element")
+#print(min(solution(A)))
