48904eb5fb1bfd734f5e552aa289f774963f501e
Linar Nasyrov <vamping111@gmail.com>
2021-08-03 01:22:10 +0300

    hw7.py has been changed and passwd,group have been added

--- hw/hw7.py
+++ hw/hw7.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import re
+print("First part of the task")
 import collections
 inputfile='/etc/passwd'
 arr=[]
@@ -12,3 +12,36 @@ for i in col:
     env=(i[0])
     cont=(i[1])
     print(env,"-",cont)
+
+print("\nSecond part of the task")
+user=[]
+group=[]
+groupfile='/etc/group'
+passwdfile='/etc/passwd'
+n=0
+grpfile=open(groupfile,mode='r',encoding='utf_8')
+for line in grpfile:
+    stage=((line.split(":"))[0].strip())
+    user.append(stage)
+    lol=((line.split(":"))[3].strip())
+    group.append(lol)
+    gig=user[int(n)],group[int(n)]
+    gig_list=(list(gig))
+    n+=1
+name=[]
+uid=[]
+b=0
+lira={}
+userfile=open(passwdfile,mode='r',encoding='utf_8')
+for line in userfile:
+    stg=((line.split(":"))[0].strip())
+    sip=((line.split(":"))[3].strip())
+    lira[stg]=sip   
+    b+=1
+lun=0
+val=0
+for i in user:
+    if group[int(lun)]!='':
+        print(user[int(val)],"-",lira[group[int(lun)]])
+    lun+=1
+    val+=1
