636548f2686e660fd6f570a09dae3331b4f04dcb
linar <vamping111@gmail.com>
2021-08-11 21:14:10 +0300

    hw23.py has been changed

--- hw/hw23.py
+++ hw/hw23.py
@@ -5,6 +5,8 @@ def matrix(*a):
 	quan = len(list(*a))
 	for i in range(int(quan)):
 		x = [x for j in range(list(*a)[i])]
-	print(x)
-total = list(map(int , (input("Enter: ").split(','))))
-matrix(total)
+	return x
+
+print(matrix([2,2]))
+
+
