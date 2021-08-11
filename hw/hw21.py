9b0aacdab83a944996950260bbb3ec3218dd8948
linar <vamping111@gmail.com>
2021-08-11 20:05:28 +0300

    hw21.py has been changed

--- hw/hw21.py
+++ hw/hw21.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
-def arr(a,b):
-  total = []
-  for i in a:
-    total.append(i)
-  for j in b:
-    total.append(j)
-  for x in sorted(total):
-    yield x
-        
-s = arr([8,96,124],[72,487,620])
-print(next(s))
-8
-print(next(s))
-72
-print(next(s))
-96
-print(next(s))
-124
-print(next(s))
-487
-print(next(s))
-620
-print(next(s))
-#Traceback (most recent call last):
-#  File "<input>", line 1, in <module>
-#StopIteration
+
+def merge(a,b):
+  for i,j in zip(a,b):
+    yield i
+    yield j
+  return merge(a,b)
+
+print(list(merge((a for a in range(1,4)),(b for b in range(2,5)))))
