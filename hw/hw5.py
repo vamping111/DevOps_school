05e65ea584e565b7227c282de2df244855448740
vamping111 <52217410+vamping111@users.noreply.github.com>
2021-09-05 20:21:54 +0300

    Update hw5.py

--- hw/hw5.py
+++ hw/hw5.py
@@ -4,7 +4,7 @@ A=[int(s) for s in inp if s.isdigit()]
 def solution(A):
     return [value for value in range(min(A),max(A)) if value not in A]
 if min(sorted(A))==1:
-    print("The value: ",(min(solution(A))), " is missed!")
+    print("The value: ",(min(solution(A))), " is missed")
 else:
-    print("The value: ",(A[0]-1), " is missed!")
+    print("The value: ",1, " is missed!")
 
