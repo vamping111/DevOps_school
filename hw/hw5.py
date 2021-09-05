4a0eee0982a07b5fca4707397b59527876639fee
vamping111 <52217410+vamping111@users.noreply.github.com>
2021-09-05 17:34:03 +0300

    The hw5.py has been changed!

--- hw/hw5.py
+++ hw/hw5.py
@@ -3,13 +3,8 @@ inp=input("Enter your values: ").split()
 A=[int(s) for s in inp if s.isdigit()]
 def solution(A):
     return [value for value in range(min(A),max(A)) if value not in A]
-try:
-    (min(solution(A)))
-    print("The value: ",(min(solution(A))), " is missed")
-except ValueError:
-    B = (A[0]-1)
-    if B == 0:
-        print(A[-1]+1)
-    elif B >= 0:
-        print(B)
+if min(sorted(A))==1:
+    print("The value: ",(min(solution(A))), " is missed!")
+else:
+    print("The value: ",(A[0]-1), " is missed!")
 
