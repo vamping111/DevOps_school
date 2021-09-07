ed2861324b69332e9e1c9b4a206cbb72da2e1208
vamping111 <52217410+vamping111@users.noreply.github.com>
2021-09-07 22:35:00 +0300

    Update hw23.py

--- hw/hw23.py
+++ hw/hw23.py
@@ -3,7 +3,8 @@
 def matrix(a):
     x = ''
     quan=len(a)
+    ent = a[0] * [x]
     if (quan == 1):
-        return [x] * a[0]
+        return ent
     else:
         return [matrix(a[1:]) for j in range(a[0])]
