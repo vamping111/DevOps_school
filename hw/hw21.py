6ae9d72ab70769317d9f965f3eced961c7fa2e09
vamping111 <52217410+vamping111@users.noreply.github.com>
2021-09-08 16:27:23 +0300

    Update hw21.py

--- hw/hw21.py
+++ hw/hw21.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-def merge(a,b):
-  for i,j in zip(a,b):
-    yield i
-    yield j
-  return merge(a,b)
+def sor(a,b):
+	def merge(a,b):
+		for i,j in zip(a,b):
+			yield i
+			yield j
+		res = merge(a,b)
+		return res
+	total = merge(a,b)
+	return sorted(total)
 
-print(list(merge((a for a in range(1,4)),(b for b in range(2,5)))))
+
+print(list(sor((a for a in (1,2,3)),(b for b in (6,7,8)))))
+print(list(sor((a for a in range(1,4)),(b for b in range(2,5)))))
+
+#[1, 2, 3, 6, 7, 8]
+#[1, 2, 2, 3, 3, 4]
