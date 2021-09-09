3ce32ff6ccb740db5c6b08ba91b60421f33db2b0
vamping111 <52217410+vamping111@users.noreply.github.com>
2021-09-10 01:26:31 +0300

    Update hw21.py

--- hw/hw21.py
+++ hw/hw21.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-
-def sor(a,b):
-	def merge(a,b):
-		for i,j in zip(a,b):
+import itertools
+import heapq
+def total(a,b):
+	def generatora(a):
+		for i in a:
 			yield i
+	def generatorb(b):
+		for j in b:
 			yield j
-		res = merge(a,b)
-		return res
-	total = merge(a,b)
-	return sorted(total)
-
-
-print(list(sor((a for a in (1,2,3)),(b for b in (6,7,8)))))
-print(list(sor((a for a in range(1,4)),(b for b in range(2,5)))))
-
-#[1, 2, 3, 6, 7, 8]
-#[1, 2, 2, 3, 3, 4]
+	res = heapq.merge(generatora(a),generatorb(b))
+	return res
